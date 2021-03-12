1445c288cb4bdce8584b1bd1e61a7ea0217c34c4
Annonymous <dev@rivne.itstep.org>
2021-03-12 20:55:56 +0200

    first commit

---
+++ Jugged_Array/JuggedArray.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Class : поле Jugged array  int [][] matrix
+// к-р приймає кількість колонок у кожному рядку як params int []+
+// властивості для читання Rows  get +
+// метод Columns(int row) ???
+// двовимірний індексатор indexator +
+// заповнення вип числами +
+// ToString()+
+
+namespace Jugged_Array
+{
+    class JuggedArray
+    {
+        int[][] matrix;
+        public JuggedArray(params int[] colums)
+        {
+            matrix = new int[colums.Length][];
+            for (int i = 0; i < colums.Length; i++)
+            {
+                matrix[i] = new int[colums[i]];
+            }
+        }
+
+        public int Rows
+        {
+            get => matrix.Length;
+        }
+        public int Colums(int row)
+        {
+            if(row<matrix.Length)
+                return matrix[row].Length;
+            throw new Exception("Error row (metod Colums)");
+        }
+        public int this[int row, int col]
+        {
+            get
+            {
+                if (row >= matrix.Length || row < 0)
+                    throw new Exception("Row error");
+
+                if (col >= matrix[row].Length || col < 0)
+                    throw new Exception("Col error");
+
+                return matrix[row][col];
+            }
+            set
+            {
+                if (row >= matrix.Length || row < 0)
+                    throw new Exception("Row error");
+
+                if (col >= matrix[row].Length || col < 0)
+                    throw new Exception("Col error");
+
+                matrix[row][col] = value;
+            }
+        }
+        public int[] this[int row]
+        {
+            get
+            {
+                if (row >= matrix.Length || row < 0)
+                    throw new Exception("Row error");
+                return matrix[row];
+            }
+            set
+            {
+                if (row >= matrix.Length || row < 0)
+                    throw new Exception("Row error");
+                matrix [row] = value;
+            }
+        }
+        public void FillRand(int first, int last)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = rand.Next(first, last+1);
+                }
+            }
+        }
+        public override string ToString()
+        {
+            string arr = "", temp;
+            foreach (var item in matrix)
+            {
+                temp = String.Join("\t", item);
+                arr += temp + "\n";
+            }
+            return arr;
+        }
+
+    }
+}
---
+++ Jugged_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Jugged_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                JuggedArray j = new JuggedArray(6, 3, 7);
+                j.FillRand(-20, 20);
+                Console.WriteLine(j.ToString());
+                Console.WriteLine(j.Rows);
+                Console.WriteLine($"Element j[1][2]: {j[1][2]}");
+                Console.WriteLine($"Count of colums in row(0): {j.Colums(0)}");
+                int[] res = j[1];
+                string resStr = String.Join('\t', res);
+                Console.WriteLine("Row[1]: "+resStr);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
